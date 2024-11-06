83f4497bb62330d2fc26c07a82d491adb1b0dbf6
Julien Vaillant <julien.vaillant91@gmail.com>
2024-11-06 13:22:15 +0100

    update recipes

--- main.js
+++ main.js
@@ -1,41 +1,65 @@
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 const app = express();
-const { connectToDatabase } = require("./utils/db");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const Movie = require("./models/Movie");
 
-require("dotenv").config();
-
-app.use(
-  cors({
-    origin: "http://localhost:3000",
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000", // Remplacez par l'URL où votre client est hébergé
+    methods: ["GET", "POST"],
     credentials: true,
-  })
-);
-
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
-
-app.use(bodyParser.json());
-
-connectToDatabase();
-
-app.post("/db", async (req, res) => {
-  const request = JSON.parse(req.body.request);
-
-  try {
-    const movies = await Movie.find(request);
-
-    res.json(movies);
-  } catch (e) {
-    res.status(500).send(e.message);
-  }
+  },
 });
 
-app.listen(3001, () => {
+const colorGenerator = function* colorGenerator() {
+  while (true) {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    yield `rgb(${r}, ${g}, ${b})`;
+  }
+};
+
+// Exemple d'utilisation
+const colors = colorGenerator();
+
+const players = new Set();
+
+io.on("connection", (socket) => {
+  console.log("Un utilisateur est connecté :", socket.id);
+
+  socket.on("register", () => {
+    const color = colors.next().value;
+    const playerData = { color, id: socket.id };
+
+    const d = [...players];
+    console.log("current", players, socket.id);
+    const find = d.find((p) => p.id === socket.id);
+    if (!find) {
+      players.add(playerData);
+    }
+
+    socket.emit("register-ok", find);
+
+    console.log("emit new player list to all");
+    socket.broadcast.emit("new-player", { players: [...players] });
+    socket.emit("new-player", { players: [...players] });
+  });
+
+  socket.on("move", (data) => {
+    socket.broadcast.emit("ennemy-move", {
+      id: socket.id,
+      x: data.x,
+      y: data.y,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Un utilisateur est déconnecté");
+  });
+});
+
+server.listen(3001, () => {
   console.log("listen on port 3001");
 });
